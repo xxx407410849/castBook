4853cc5bc49c48aecdeef205f9ed7ed8151ed383
xxx407410849 <407410849@qq.com>
2018-09-21 03:26:16 +0800

    18-9-21
    Signed-off-by: xxx407410849 <407410849@qq.com>

--- MapRedux/mapShowCast.jsx
+++ MapRedux/mapShowCast.jsx
@@ -1,15 +1,6 @@
 import {connect} from 'react-redux'
 import ShowCast from '../HOC/showCast.jsx'
 const mapStateToProps = (state) => {
-<<<<<<< HEAD
-    return {
-        dataList : state.get('Casts')
-    }
-}
-
-const IndexShowCast = connect(
-    mapStateToProps,
-=======
     let pieData = [];
     state.get('types').forEach((item)=>{
         if(item.get('value') != 0){
@@ -29,7 +20,6 @@ const mapDispatchToProps = (dispatch) => {
 const IndexShowCast = connect(
     mapStateToProps,
     mapDispatchToProps
->>>>>>> testChange3
 )(ShowCast)
 
 module.exports = IndexShowCast
